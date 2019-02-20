7f9b52aaf0c8a10d635cc5cd4a989af481effa4c
mingrammer <mingrammer@gmail.com>
2019-02-20 11:58:39 +0900

    Brew formula update for dynamodb-toolkit version v0.0.4

--- dynamodb-toolkit.rb
+++ dynamodb-toolkit.rb
@@ -2,9 +2,9 @@
 class DynamodbToolkit < Formula
   desc "A command line toolkit for aws dynamodb"
   homepage "https://github.com/mingrammer/dynamodb-toolkit"
-  url "https://github.com/mingrammer/dynamodb-toolkit/releases/download/v0.0.3/dynamodb-toolkit_0.0.3_darwin_amd64.tar.gz"
-  version "0.0.3"
-  sha256 "75e3d7182b956ecb08c4864b5354cf81d82613500b581a82d9eae89e6484f8bb"
+  url "https://github.com/mingrammer/dynamodb-toolkit/releases/download/v0.0.4/dynamodb-toolkit_0.0.4_darwin_amd64.tar.gz"
+  version "0.0.4"
+  sha256 "7e5c6098028cb6ef1851a6f8f465754400c13b8319ba519df892273dde73976a"
 
   def install
     bin.install "dynamotk"
